cc138236f59163dd2f98fa44eade465966aafa24
Valentin Ivanov <valen.ivanov@gmail.com>
2018-04-16 17:19:01 -0400

    progressing through the course

--- dictionary/lib/dictionary.ex
+++ dictionary/lib/dictionary.ex
@@ -1,18 +1,16 @@
 defmodule Dictionary do
-  @moduledoc """
-  Documentation for Dictionary.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Dictionary.hello
-      :world
-
-  """
   def hello do
-    IO.puts "Hello world!!"
+    IO.puts "Hello world!!!"
+  end
+
+  def word_list() do
+    "assets/words.txt"
+    |> File.read!()
+    |> String.split(~r/\n/)    
+  end
+
+  def random_word() do
+    word_list()
+    |> Enum.random()
   end
 end
