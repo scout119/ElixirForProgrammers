2fa700708345aa7f41a176dd428fe1bbb1bf20ec
Valentin Ivanov <valen.ivanov@gmail.com>
2018-04-17 17:12:04 -0400

    Coding along

---
+++ hangman/lib/hangman/game.ex
@@ -0,0 +1,27 @@
+defmodule Hangman.Game do
+  
+  defstruct(
+    turns_left:  7,
+    game_state: :initializing,
+    letters:    []
+  )
+
+  def new_game() do
+    %Hangman.Game{
+      letters: Dictionary.random_word() |> String.codepoints()
+  }
+  end
+
+  def make_move(game = %{game_state: state}, _guess) when state in [:won, :lost] do
+     { game, tally(game) }
+  end
+
+  def make_move(game, guess) do
+    
+  end
+
+  def tally(game) do
+    123
+  end
+
+end
---
+++ hangman/test/game_test.exs
@@ -0,0 +1,20 @@
+defmodule GameTest do
+  use ExUnit.Case
+
+  alias Hangman.Game
+
+  test "new_game returns structure" do
+    game = Game.new_game()
+    assert game.turns_left == 7
+    assert game.game_state == :initializing
+    assert game.letters |> length > 0    
+  end
+
+  test "state isn't changed for :won or :lost game" do
+    for state <- [:won, :lost] do
+      game = Game.new_game() |> Map.put(:game_state, state)
+      assert { ^game, _} = Game.make_move(game, "x")
+    end 
+  end
+  
+end
